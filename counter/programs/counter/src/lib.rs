2cf4944a801b971efebd12388577e437d37a6081
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-11-02 21:37:18 +0530

    changed the cluster to local net

--- counter/programs/counter/src/lib.rs
+++ counter/programs/counter/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("XUfg1HSnzG2Yp9WV1j4fbMFUMujQetGKeG4XM1J3S3Z");
 
 #[program]
 pub mod counter {
