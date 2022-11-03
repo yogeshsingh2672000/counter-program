7df593828d76055b7ac8cbe338e77284610879eb
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-11-03 16:13:27 +0530

    program and frontend-client

---
+++ counter-program/programs/counter-program/src/lib.rs
@@ -0,0 +1,50 @@
+use anchor_lang::prelude::*;
+
+declare_id!("7WDFmqyzVeCa34a6J4jyGTUPLmvsy6a3ZrHsRGG91V1m");
+
+#[program]
+pub mod counter {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+        Ok(())
+    }
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
+        Ok(())
+    }
+    pub fn decrement(ctx: Context<Decrement>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count -= 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8+16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[derive(Accounts)]
+pub struct Decrement<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
